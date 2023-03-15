114dfe15633a18b2dd06c94ab2a8b404d1c45054
mas-360 <126551615+mas-360@users.noreply.github.com>
2023-03-15 14:25:37 +0200

    Update masapp.py

--- masapp.py
+++ masapp.py
@@ -45,7 +45,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
-local_css(r"C:/Users/27823/MASINSIGHT/masapp/style/style.CSS")
+local_css('C:/Users/27823/MASINSIGHT/masapp/style/style.CSS')
 
 #---LOAD ASSETS---
 lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_rznvYX6xOF.json")
